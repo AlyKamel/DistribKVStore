7ad3a7d85072af8ddebc6a73feeec2563884a0df
Aly Kamel <>
2020-01-26 15:42:14 +0100

    impoved the performance test

--- src/test/java/de/tum/i13/ClientThread.java
+++ src/test/java/de/tum/i13/ClientThread.java
@@ -1,124 +1,75 @@
 package de.tum.i13;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.List;
 import de.tum.i13.client.ClientLibrary;
 
 public class ClientThread extends Thread {
-	String pref; //prefix for prints on console
-	String path; //directory path to the .txt for commands
-	ClientLibrary cl = new ClientLibrary();
-	String first;
-	int mode;
-	
-	public ClientThread(String id, String addr, int port, String path) {
-		super();
-		pref = "Client "+id+": ";
-		this.path = path;
-		try {
-			System.out.println(pref+"START");
-			System.out.println(pref+"addr ="+addr+" port ="+port);
-			System.out.println("CONN "+cl.connect(addr, port));
-			System.out.println("USER "+cl.setUsername("Client"+id));
-			
-		} catch (IOException e) {
-			System.out.println(pref+"Connection error");
-		}
-	}
-	
-	@Override
-	public void run() {
-		mode = 0; //=put
-		System.out.println("\n"+pref+"DOING PUT");
-		test();
-		mode = 1; //=get
-		System.out.println("\n"+pref+"DOING GET");
-		test();
-		mode = 2; //=del
-		System.out.println("\n"+pref+"DOING DEL");
-		test();
-	}
-	
-	private void test() {
-		try {
-			File file = new File(path);
-    	BufferedReader read = new BufferedReader(new FileReader(file));
-    	System.out.println(pref+"Opened file");
-    	String line = read.readLine();
-    	while(line != null) {
-    		System.out.println(pref+doLine(line));
-    		//doLine2(line);
- 	      line = read.readLine();
-    	}
-	    read.close();
-    } catch (FileNotFoundException e) {
-			System.out.println(pref+"file not found");
-		} catch (IOException e) {
-    	System.out.println(pref+"line read fail or doLine fail");
+  private final String pref;
+  private final ClientLibrary cl = new ClientLibrary();
+  private int mode;
+  private final List<String> input;
+
+  public ClientThread(int clientNum, String addr, int port, List<String> input) {
+    this.input = input;
+    pref = "Client " + clientNum + ": ";
+    try {
+      cl.connect(addr, port);
+      cl.setUsername("client" + clientNum);
+    } catch (IOException e) {
+      System.out.println(pref + "Connection error");
     }
-	}
-	
-	private String doLine(String line) {
-		String[] comp = line.split("\\s+",2);
-		System.out.println("Comp "+comp[0]+" "+comp[1]);
-		switch(mode) {
-		case 0: 
-			try {
-				return cl.putRequest(comp[0], "\"" + comp[1]+ "\"");
-			} catch (IOException e) {
-				return "put fail";
-			}
-		case 1:
-			try {
-				return cl.getRequest(comp[0]);
-			} catch (IOException e) {
-				return "get fail";
-			}
-		case 2:
-			try {
-				return cl.deleteRequest(comp[0]);
-			} catch (IOException e) {
-				return "delete fail";
-			}
-		default:
-			return "unknown command, not put, get or del";
-		}
-	}
-	
-	private void doLine2(String line) {
-		String[] comp = line.split("\\s+",2);
-		switch(mode) {
-		case 0: 
-			try {
-				assertEquals("SUCCES",cl.putRequest(comp[0], "\"" + comp[1]+ "\""));
-				break;
-			} catch (IOException e) {
-				System.out.println("put fail");
-				break;
-			}
-		case 1:
-			try {
-				assertEquals(comp[1], cl.getRequest(comp[0]));
-				break;
-			} catch (IOException e) {
-				System.out.println("get fail");
-				break;
-			}
-		case 2:
-			try {
-				assertEquals("SUCCESS", cl.deleteRequest(comp[0]));
-			} catch (IOException e) {
-				System.out.println("delete fail");
-				break;
-			}
-		default:
-			System.out.println("unknown command, not put, get or del");
-		}
-	}
+  }
+
+  @Override
+  public void run() {
+    System.out.println("\n" + pref + "DOING PUT");
+    test(0);
+    System.out.println("\n" + pref + "DOING GET");
+    test(1);
+    System.out.println("\n" + pref + "DOING DEL");
+    test(2);
+  }
+
+  private void test(int mode) {
+    this.mode = mode;
+    for (String line : input) {
+      String result = doLine(line);
+      // System.out.println(pref + result);
+      
+      // try waiting before sending next command
+//    try {
+//    Thread.sleep(40);
+//  } catch (InterruptedException e) {
+//    e.printStackTrace();
+//  }
+    }
+  }
+
+  private String doLine(String line) {
+    String result = null;
+    String[] comp = line.split("\\s+", 2);
+    try {
+      switch (mode) {
+        case 0:
+          result = cl.putRequest(comp[0], "\"" + comp[1] + "\"");
+          assertEquals("SUCCESS", result);
+          return result;
+        case 1:
+          result = cl.getRequest(comp[0]);
+          assertEquals(comp[1], result);
+          return result;
+        case 2:
+          result = cl.deleteRequest(comp[0]);
+          assertEquals("SUCCESS", result);
+          return result;
+        default:
+          return "unknown command, not put, get or del";
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "ioexception";
+    }
+  }
 }
--- src/test/java/de/tum/i13/Testsimple.java
+++ src/test/java/de/tum/i13/Testsimple.java
@@ -4,75 +4,74 @@ import static de.tum.i13.Util.createECSServer;
 import static de.tum.i13.Util.createServer;
 import static de.tum.i13.shared.ServerUtility.getFreePort;
 import static de.tum.i13.shared.ServerUtility.getSocketAddress;
-
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import de.tum.i13.server.ecs.MainECS;
-import de.tum.i13.server.threadperconnection.Main;
-
 public class Testsimple {
-  
-	int cCount = 1; // number of clients threads
 
-	private static final int serverCount = 2;
+  private static final int clientCount = 1;
+  private static final int serverCount = 4;
   private static int[] ports = new int[serverCount];
   private static final String address = "127.0.0.1";
   private static final Thread[] servers = new Thread[serverCount];
-	
-  @BeforeAll
-  public static void setup() throws IOException, InterruptedException{
-    Logger.getLogger(MainECS.class.getName()).setLevel(Level.ALL);
-    Logger.getLogger(Main.class.getName()).setLevel(Level.ALL);
+  private static List<String> input;
 
+  // currently works for:
+  // 1S1C, 2S1C, 3S1C
+ 
+  
+  @BeforeAll
+  public static void setup() throws IOException, InterruptedException {
+    // set input
+    Path path = Path.of("bi320.txt");
+    input = Files.readAllLines(path);
+    
+    // start ecs
     int ecsport = getFreePort(address);
     String bootstrap = getSocketAddress(address, ecsport);
     Thread ecs = createECSServer(address, ecsport);
     ecs.start();
-    
+
+    // start servers
     for (int i = 0; i < serverCount; i++) {
       ports[i] = getFreePort(address);
       servers[i] = createServer(address, ports[i], bootstrap);
       servers[i].start();
-      Thread.sleep(3000);
+      Thread.sleep(2000);
     }
- 
-    Thread.sleep(2000);  
-  } 
-    
- @Test
- public void TestSimple() {
-	 System.out.println("###START###\n");
-	 
-	 String addr = address; //server
-	 int port = ports[0]; //server
-	 String path = "C:\\Users\\Ricardo\\Desktop\\Cloud DB\\milestone5additional\\src\\test\\java\\de\\tum\\i13\\inputs";
-	 ClientThread[] clients = new ClientThread[cCount];
 
-	 //in the beginning of the test
-	 for(int i = 0; i < cCount; i++) {
-		 clients[i] = new ClientThread(""+i, addr, port, path+"\\"+i+".txt"); 
-	 }
-	 
-	 System.out.println("\nStart Clients:\n");
-	 for(int i = 0; i < cCount; i++) {
-		 clients[i].start();
-		 System.out.println("Client"+i+": Start");
-	 }
-	 
-	 for(int i = 0; i < cCount; i++) {
-		 try {
-			clients[i].join();
-		} catch (InterruptedException e) {
-			System.out.println("Client Thread fail");
-		}
-		 System.out.println("Client"+i+": End");
-	 }
-	 
-	 System.out.println("\n###END###\n");
- }
+    Thread.sleep(2000);
+  }
+
+  @Test
+  public void TestSimple() {
+    System.out.println("###START###\n");
+    
+    // setup clients
+    ClientThread[] clients = new ClientThread[clientCount];
+    for (int i = 0; i < clientCount; i++) {
+      clients[i] = new ClientThread(i, address, ports[0], input);
+    }
+
+    // start clients
+    for (int i = 0; i < clientCount; i++) {
+      clients[i].start();
+      System.out.println("Client" + i + ": Start");
+    }
+
+    // wait for clients
+    for (int i = 0; i < clientCount; i++) {
+      try {
+        clients[i].join();
+      } catch (InterruptedException e) {
+        System.out.println("Client " + i + ": Fail");
+      }
+      System.out.println("Client" + i + ": End");
+    }
+    System.out.println("\n###END###\n");
+  }
 }
