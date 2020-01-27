8a29f0d3f918c4549aec890988b11cad2e1c4613
ga84vax <ga84vax@mytum.de>
2020-01-27 21:30:56 +0100

    update slides

--- src/main/java/de/tum/i13/server/kv/KVStorageSystem.java
+++
@@ -1,9 +0,0 @@
-package de.tum.i13.server.kv;
-
-public interface KVStorageSystem {
-  public ServerStatus put(String key, String value);
-
-  public String get(String key);
-
-  public ServerStatus delete(String key);
-}
--- src/main/java/de/tum/i13/server/kv/caching/CachingStrategy.java
+++
@@ -1,5 +0,0 @@
-package de.tum.i13.server.kv.caching;
-
-public enum CachingStrategy {
-  FIFO, LRU, LFU;
-}
